219cc105253f7432313756aaf6d84e59b55c2146
THEERROR2002 <72090888+THEERROR2002@users.noreply.github.com>
2022-04-17 19:01:08 +0200

    Update features.cpp

--- BigBaseV2/src/features.cpp
+++ BigBaseV2/src/features.cpp
@@ -3,6 +3,8 @@
 #include "logger.hpp"
 #include "natives.hpp"
 #include "script.hpp"
+#include "gta_util.hpp"
+#include "helpers/player_info.h"
 
 namespace big
 {
@@ -16,6 +18,41 @@ namespace big
 
 	void features::run_tick()
 	{
+		ULONGLONG now = GetTickCount64();
+
+		g_local_player = gta_util::get_local_ped();
+
+		if (g_player_info.player_id != PLAYER::PLAYER_ID() || g_player_info.player_id != PLAYER::PLAYER_PED_ID())
+		{
+			g_player_info.player_id = PLAYER::PLAYER_ID();
+			g_player_info.player_ped = PLAYER::PLAYER_PED_ID();
+		}
+
+		for (int i = 0; i < sizeof(tick_conf) / sizeof(ULONGLONG); i++)
+		{
+			if (now - ticks[i] > tick_conf[i])
+			{
+				g_current_tick_job = i;
+
+				switch (g_current_tick_job)
+				{
+				case 0:
+					//50ms
+					break;
+				case 1:
+					//250ms
+					break;
+				case 2:
+					//2000ms
+					break;
+				case 3:
+					//25003ms
+					break;
+				}
+
+				ticks[i] = now;
+			}
+		}
 	}
 
 	void features::script_func()
@@ -24,6 +61,10 @@ namespace big
 		{
 			TRY_CLAUSE
 			{
+				if (!g_first)
+				{
+					g_first = true;
+				}
 				run_tick();
 			}
 			EXCEPT_CLAUSE
