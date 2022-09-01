bad25b5508ce327b7f294d83e267d0dcd741eb8c
Narweex <68287337+Narweex@users.noreply.github.com>
2022-09-01 19:34:48 +0300

    Add files via upload

--- BigBaseV2/src/auth/auth.cpp
+++ BigBaseV2/src/auth/auth.cpp
@@ -1,8 +1,8 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #define jwtkey "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmt0Muy8v3rH4qU5Bj7Ae\nf0qdNe/rBkzsWYr23kbja1ZU78j+3Ou6MZtXPIWSpZChFKeeL/9c66amDG4kWd4U\n6ROtrQaBDSTZk4iN95UNZIPR0kotNygxNuv/3Yvqq119gMoC9aVjmZE6OS9Mviof\nW5ysZtWzK7He36ZQbMTsiHJz3PIOU99Pw2Nl6tQid56N2/eue1X2gvbTYtXTUL2X\nDLQxhxOoIAKd8FhuWjoKaMbEXgU1Qt1hSpocxtF9F+bEuSs08dZa1WRkVTl6vQp5\njkhVhrZfKlNQk+vtbF+tjnJZS2fhWHzIr+FVbGJlGZBZkhX2nSB3eChdNMcZTU5o\nIwIDAQAB\n-----END PUBLIC KEY-----" //encrypt this
-
+//#define LAZY_IMPORTER_HARDENED_MODULE_CHECKS
 #define WIN32_LEAN_AND_MEAN
-
+#include <utility>
 #include <iostream>
 #include <windows.h>
 #include <string>
@@ -17,12 +17,12 @@
 #include <httplib.h>
 #include <winsock2.h>
 #include <ws2def.h>
-
+#include "lazy_importer.hpp"
 #include <urlmon.h>
 #include <conio.h>
 #include <sys/stat.h>
 #include "auth/includes/anti_debug.hpp"
-
+#include <stdio.h>
 #include <WinBase.h>
 #include "auth/includes/httplib.h"
 #include "auth/includes//curl/curl.h"
@@ -40,23 +40,18 @@ using namespace httplib;
 using namespace jwt;
 
 
-void authenticate()
-{
 
-    return;
-   
-}
 
-int loginuser()
+int loginuser(std::string username, std::string password)
 {
-    authenticate();
+    //authenticate(username, password);
     return 1;
 }
 
 
 void auth::auth(std::string username, std::string password)
 {
-   
+    LI_FN(OutputDebugStringA)("hello world");
     std::string login_file = getenv("APPDATA");
     login_file += "\\Zyko\\autologin.json";
     const char* logfil = login_file.c_str();
@@ -89,7 +84,7 @@ void auth::auth(std::string username, std::string password)
 
     //verify more then the result, add integrity checks and encrypt all strings in this file
     if (payloadJson["result"] == "invalid credentials") {
-        LOG(INFO) << "Invalid credentials!";
+        LOG(INFO) << xorstr_("Invalid credentials!");
         exit(0);
         std::string login_file = getenv("APPDATA");
         login_file += "\\Zyko\\autologin.json";
@@ -97,20 +92,20 @@ void auth::auth(std::string username, std::string password)
         
 
     }
-    if (payloadJson["result"] == "invalid hwid") {
-        LOG(INFO) << "You Got Bad HWID";
+    else if (payloadJson["result"] == "invalid hwid") {
+        LOG(INFO) << xorstr_("You Got Bad HWID");
         Sleep(10000);
-        exit(0);
+       // exit(0);
     }
-    if (payloadJson["result"] == "banned") {
-        LOG(INFO) << "you got banned lol";
-       exit(0);
+    else  if (payloadJson["result"] == "banned") {
+        LOG(INFO) << xorstr_("you got banned lol");
+       //exit(0);
     }
-    if (payloadJson["result"] == "success") {
-        LOG(INFO) << "You dont have license radeemed";
-        exit(0);
+    else if (payloadJson["result"] == "success") {
+        LOG(INFO) << xorstr_("You dont have license radeemed");
+       // exit(0);
     }
-    if (payloadJson["result"] == "successpaid") {
+    else  if (payloadJson["result"] == "successpaid") {
         //LOG(INFO) << "Succesfully Logged in";
         auth::login = true;
        // LOG(INFO) << "Welcome" << username1;
@@ -119,25 +114,54 @@ void auth::auth(std::string username, std::string password)
 
 
 
-void auth::connect()
+void auth::connect(std::string username, std::string password)
 {
 }
-void auth::authenticate()
+
+
+
+int auth::authenticate()
 {
+   
+    void (*authenticate)(std::string username, std::string password);
+    authenticate = auth::log_in;
+    authenticate(username, password);
+    return 1;
 }
-void auth::verify()
+
+int auth::verify(std::string username, std::string password)
 {
+   
+    auth::authenticate();
+    void (*authenticate)(std::string username, std::string password);
+    authenticate = auth::auth;
+    authenticate(username, password);
+    return 1;
 }
-void auth::log_in()
+
+
+
+
+
+
+
+
+
+void auth::log_in(std::string username, std::string password)
 {
+    void (*authenticate)(std::string username, std::string password);
+    authenticate = auth::unlock;
+    authenticate(username, password);
+    return;
 }
-void auth::unlock()
+void auth::unlock(std::string username, std::string password)
 {
+    return;
+  
 }
 void auth::nigger123::authentication(std::string username, std::string password)
 {
 }
-void authenticate();
 
 
 int loginuser();
--- BigBaseV2/src/auth/auth.hpp
+++ BigBaseV2/src/auth/auth.hpp
@@ -1,23 +1,25 @@
-//#pragma once
-//
-//static char username[28];
-//static char password[28];
+#pragma once
+#include "common.hpp"
+
+
+static char username[28];
+static char password[28];
 namespace auth
 {
-//	void auth(std::string username, std::string password);
-//	void connect();
-//	void authenticate();
-//	void verify();
-//	void log_in();
-//	void unlock();
-//
-//	class nigger123
-//	{
-//	protected:
-//		void authentication(std::string username, std::string password);
-//	};
-	inline bool login = false;
-//	
-//
+	void auth(std::string username, std::string password);
+	void connect(std::string username, std::string password);
+	int authenticate();
+	int verify(std::string username, std::string password);
+	void log_in(std::string username, std::string password);
+	void unlock(std::string username, std::string password);
+
+	class nigger123
+	{
+	protected:
+		void authentication(std::string username, std::string password);
+	};
+inline bool login = false;
+	
+
 }
-//
+
