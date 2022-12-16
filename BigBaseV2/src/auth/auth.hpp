f4e7a4ce53ee798a7eb58c27b7c297455b65292c
Narweex <68287337+Narweex@users.noreply.github.com>
2022-12-16 16:48:42 +0100

    Add files via upload

--- BigBaseV2/src/auth/auth.hpp
+++ BigBaseV2/src/auth/auth.hpp
@@ -10,19 +10,37 @@ private:
 public:
 	std::string m_username_check;
 	std::string m_password_check;
-	Auth(const std::string& username, const std::string& password)
-		:m_username(username), m_password(password), m_username_check(username), m_password_check(password)
-	{
-		
-		auth(m_username, m_password);
-	};	
 	Auth()
 	{
-		LOG(INFO) << m_username_check << m_password_check;
-		auth(m_username_check, m_password_check);
-	};
-	//void Authenticate(const std::string& username, const std::string& password);
-	void auth(const std::string& username, const std::string& password);
+		static int nigger;
+		
+		
+			std::string login_file = getenv("APPDATA");
+			login_file += "\\Zyko\\autologin.json";
+			const char* logfil = login_file.c_str();
+
+			std::string username;
+			std::string password;
+			nlohmann::json login;
+
+			std::ifstream login_file1(login_file);
+			login_file1 >> login;
+
+			username = login["Username"];
+			password = login["Password"];
+			
+		
+		nigger = auth(username, password);
+	
+		if (nigger != 1)
+		{
+			exit(420);
+		}
+	};	
+	
+	
+	int auth(const std::string& username, const std::string& password);
+	
 public:
 	
 };
@@ -33,7 +51,8 @@ namespace auth
 {
 	static char username[28];
 	static char password[28];
-	inline bool login = false;
-	static int login1 = 0;
+	inline bool login = true;
+	inline bool login1 = true;
+
 }
 
